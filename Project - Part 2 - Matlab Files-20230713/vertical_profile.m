3a79e1a3cdfe92eadd04696ecf6eaf0456371949
LordWeissbrot <64602426+LordWeissbrot@users.noreply.github.com>
2023-08-03 18:37:03 +0200

    Commented and simplified code
    Part 1 and part 2 code commented via chat and removed unwanted lines

--- Project - Part 2 - Matlab Files-20230713/vertical_profile.m
+++ Project - Part 2 - Matlab Files-20230713/vertical_profile.m
@@ -12,6 +12,7 @@
 %     7. Then, climb at constant Mach (M = 0.78) till cruising altitude (FL350)
 % 
 
+% Cleaning workspace
 clear all
 close all
 clc
@@ -24,16 +25,17 @@ G_CONST	= 9.80665;
 M2NM = (1/1852.0);
 MPS2KTS = (3.6/1.852);
 
-%   Global variables
+% Global variables
 global ac Tratio  accel h_target CAS_target
 
-%   Load the aircraft model for a A320
+% Load the aircraft model for a A320
 [ac] = load_aircraft_bada('B773__.OPF');
 
+% Define event options
 altitude_options = odeset('Events', @altitude_event);
 cas_options = odeset('Events', @cas_event);
 
-%   Set up initial conditions
+% Initial conditions
 [rho, p, temp, soundSpeed] = Atmos(0);
 CAS  = 176*KTS2MPS; % V2 + 10
 t(1) = 0;
@@ -42,220 +44,178 @@ h(1) = 0;
 r(1) = 0;
 w(1) = ac.mass.m_ref*G_CONST;
 
-% -------------------------------------------------------
 % Stage 1 - First constant climb to 1500ft
-% -------------------------------------------------------
 Tratio      = 1.0;
 h_target    = 1500*FT2M; % set stopping event
 accel       = 0; % constant CAS profile
-
-% Find solution to the differential equation
 [tx,x] = ode45(@climb_profile, [0 300], [V(end), h(end), r(end), w(end)], altitude_options);
-
-%   Extract height profile
 t = [t; tx];
 V = [V; x(:,1)];
 h = [h; x(:,2)];
 r = [r; x(:,3)];
 w = [w; x(:,4)];
 
-% -------------------------------------------------------
 % Stage 2 - Second constant climb to 3000ft
-% -------------------------------------------------------
 Tratio      = 0.85;
 h_target    = 3000*FT2M; % set stopping event
 accel       = 0; % constant CAS profile
-
-% Find solution to the differential equation
 [tx,x] = ode45(@climb_profile, [0 300], [V(end), h(end), r(end), w(end)], altitude_options);
-
-%   Extract height profile
 t = [t; tx];
 V = [V; x(:,1)];
 h = [h; x(:,2)];
 r = [r; x(:,3)];
 w = [w; x(:,4)];
 
-% -------------------------------------------------------
 % Stage 3 - First accelerated climb till 250kts
-% -------------------------------------------------------
 Tratio      = 1;
 CAS_target  = 250*KTS2MPS; % set stopping event
 accel       = 1; % accelerated CAS profile
-
-% Find solution to the differential equation
 [tx,x] = ode45(@climb_profile, [0 300], [V(end), h(end), r(end), w(end)], cas_options);
-
-%   Extract height profile
 t = [t; tx];
 V = [V; x(:,1)];
 h = [h; x(:,2)];
 r = [r; x(:,3)];
 w = [w; x(:,4)];
 
-% -------------------------------------------------------
 % Stage 4 - Third constant climb to 10000ft
-% -------------------------------------------------------
 Tratio      = 0.85;
 h_target    = 10000*FT2M; % set stopping event
 accel       = 0; % constant CAS profile
-
-% Find solution to the differential equation
 [tx,x] = ode45(@climb_profile, [0 300], [V(end), h(end), r(end), w(end)], altitude_options);
-
-%   Extract height profile
 t = [t; tx];
 V = [V; x(:,1)];
 h = [h; x(:,2)];
 r = [r; x(:,3)];
 w = [w; x(:,4)];
 
-% -------------------------------------------------------
 % Stage 5 - Second accelerated climb till 300kts
-% -------------------------------------------------------
 Tratio      = 0.85;
 CAS_target  = 300*KTS2MPS; % set stopping event
 accel       = 1; % accelerated CAS profile
-
-% Find solution to the differential equation
 [tx,x] = ode45(@climb_profile, [0 1000], [V(end), h(end), r(end), w(end)], cas_options);
-
-%   Extract height profile
 t = [t; tx];
 V = [V; x(:,1)];
 h = [h; x(:,2)];
 r = [r; x(:,3)];
 w = [w; x(:,4)];
 
-% -------------------------------------------------------
 % Stage 6 - Third constant climb to crossover altitude
-% -------------------------------------------------------
-
-%   Compute the crossover altitude
 [h_crossover] = compute_crossover_altitude(300*KTS2MPS, 0.78);
-
 Tratio      = 0.85;
 h_target    = h_crossover; % set stopping event
 accel       = 0; % constant CAS profile
-
-% Find solution to the differential equation
 [tx,x] = ode45(@climb_profile, [0 1000], [V(end), h(end), r(end), w(end)], altitude_options);
-
-%   Extract height profile
 t = [t; tx];
 V = [V; x(:,1)];
 h = [h; x(:,2)];
 r = [r; x(:,3)];
 w = [w; x(:,4)];
 
-% -------------------------------------------------------
-% Stage 7 - Third constant climb to crossover altitude
-% -------------------------------------------------------
+% Stage 7 - Climb at constant Mach to cruising altitude (FL350)
 Tratio      = 0.85;
 h_target    = 35000.0*FT2M; % set stopping event
 accel       = 2;  % constant Mach profile
-
-% Find solution to the differential equation
 [tx,x] = ode45(@climb_profile, [0 1000], [V(end), h(end), r(end), w(end)], altitude_options);
-
 t = [t; tx+t(end)];
 V = [V; x(:,1)];
 h = [h; x(:,2)];
 r = [r; x(:,3)];
 w = [w; x(:,4)];
 
-%% --------------- VISUALIZATION PART ----------------------
-
-Vcas = [NaN];
+% Visualization part
+Vcas = [NaN];shadow
 M = [NaN];
 for ii=1:length(V)
-    %   Obtain pressure and density profile for the height profile
+    % Obtain pressure and density profile for the height profile
     [rho, p, temp, soundSpeed] = Atmos(h(ii));
     Vcas(ii) = TAStoCAS(V(ii), rho, p);
     M(ii) = TAStoM(V(ii), rho, p);
 end
 
-% Show results
-figure(1)
-
-x = 32;
-y = 18;
+% Define figure options
 options.units = 'centimeters';
-options.FontMode = 'Fixed'; %This line and next are the important bit
-options.format = 'eps'; %or whatever options you'd like
+options.FontMode = 'Fixed';
+options.format = 'eps';
 options.FontName = 'arial';
 options.FixedFontSize = 20;
 options.Renderer = 'painters';
-options.Width = x;
-options.Height = y;
-set(gcf,'PaperOrientation','portrait','units','centimeters','PaperSize',[x y],'PaperPosition',[0 0 x y])
+options.Width = 32;
+options.Height = 18;
 
+% Plotting and exporting figures
+figure(1)
 plot(r*M2NM,h*M2FT/100,'LineWidth',2);
-grid on
-set(gca,'FontSize',16,'FontWeight','bold');
-set(gca,'GridAlpha',0.4);
 xlabel('Range [NM]', 'FontSize', 16, 'FontWeight', 'bold')
 ylabel('Flight Level', 'FontSize', 16, 'FontWeight', 'bold');
-% title('Climb vs Range', 'FontSize', 16, 'FontWeight', 'bold');
-
-ax = gca;
-ax.FontSize = 15;  
-
 hgexport(gcf,'H_r.eps',options);
 saveas( gcf , 'H_r.png' );
 
 figure(2)
 
+% Define figure options
 x = 32;
 y = 18;
 options.units = 'centimeters';
-options.FontMode = 'Fixed'; %This line and next are the important bit
-options.format = 'eps'; %or whatever options you'd like
+options.FontMode = 'Fixed';
+options.format = 'eps';
 options.FontName = 'arial';
 options.FixedFontSize = 20;
 options.Renderer = 'painters';
 options.Width = x;
 options.Height = y;
+
+% Set figure properties
 set(gcf,'PaperOrientation','portrait','units','centimeters','PaperSize',[x y],'PaperPosition',[0 0 x y])
 
+% Plot TAS and CAS against range
 plot(r*M2NM,V*MPS2KTS,'LineWidth',2); grid on; hold on
 plot(r*M2NM,Vcas*MPS2KTS,'LineWidth',2); grid on; hold on
+
+% Set graph properties
 set(gca,'FontSize',16,'FontWeight','bold');
 set(gca,'GridAlpha',0.4);
 xlabel('Range [NM]', 'FontSize', 16, 'FontWeight', 'bold')
 ylabel('Airspeed [kts]', 'FontSize', 16, 'FontWeight', 'bold');
-% title('Airspeed vs Range', 'FontSize', 16, 'FontWeight', 'bold');
 legend('TAS', 'CAS', 'FontSize', 16, 'Location','southeast')
 
+% Set axis font size
 ax = gca;
 ax.FontSize = 15;  
 
+% Export the figure
 hgexport(gcf,'V_r.eps',options);
 saveas( gcf , 'V_r.png' );
 
 figure(3)
 
+% Define figure options
 x = 32;
 y = 18;
 options.units = 'centimeters';
-options.FontMode = 'Fixed'; %This line and next are the important bit
-options.format = 'eps'; %or whatever options you'd like
-options.FixedFontSize = 20;
+options.FontMode = 'Fixed';
+options.format = 'eps';
 options.FontName = 'arial';
+options.FixedFontSize = 20;
 options.Renderer = 'painters';
 options.Width = x;
 options.Height = y;
+
+% Set figure properties
 set(gcf,'PaperOrientation','portrait','units','centimeters','PaperSize',[x y],'PaperPosition',[0 0 x y])
 
+% Plot Mach number against range
 plot(r*M2NM,M,'LineWidth',2); grid on; hold on
+
+% Set graph properties
 set(gca,'FontSize',16,'FontWeight','bold');
 set(gca,'GridAlpha',0.4);
 xlabel('Range [NM]', 'FontSize', 16, 'FontWeight', 'bold')
 ylabel('Mach number', 'FontSize', 16, 'FontWeight', 'bold');
-% title('Airspeed vs Range', 'FontSize', 16, 'FontWeight', 'bold');
 
+% Set axis font size
 ax = gca;
 ax.FontSize = 15;  
 
+% Export the figure
 hgexport(gcf,'M_r.eps',options);
 saveas( gcf , 'M_r.png' );
--- process_lateral_profile.m
+++ process_lateral_profile.m
@@ -1,195 +1,168 @@
+% process_lateral_profile.m
+% Template for processing lateral profile data for flight management and procedure design
+%
+% Inputs:
+%   - all_wpts: a structure containing navigation database data including waypoint names, 
+%     geographical information and various constraints.
+%
+% Outputs:
+%   - all_wpts_out: Updated navigation data
+%   - lat: Latitude values
+%   - lon: Longitude values
+%   - lat_wpts: Waypoint latitude values
+%   - lon_wpts: Waypoint longitude values
+%
+% Copyright (c) 2021 TU Berlin FF
 
-
-%
-%   process_lateral_profile.m  - TEMPLATE 
-%
-%   Flight Management and Procedure Design
-%
-%   Input - all_wpts: structure with the following members
-%
-%           line            = line from the navigation database file
-%           leg_type        = path termination type (e.g. IF, TF etc.)
-%           name            = name of the waypoint;
-%           lat             = latitude of the waypoint if available [deg];
-%           lon             = longitude of the waypoint if available [deg];
-%           alt_constraint  = altitude constraint 
-%                             (1: at, 2: above, 3:below, 4: between)
-%           alt_top         = top altitude of constraint (top if it is a
-%                             between constraint)
-%           alt_bottom      = bottom altitude of the altitude window (if between)
-%           spd_constraint  = speed constraint (1 = at orn below)
-%           spd_value       = value of constraint
-%           crs             = course in case of an CF leg
-%           center_lat      = latitude of center of radius in case of a RF leg
-%           center_lon      = longitude of center of radius in case of a RF leg
-%
-%   Copyright (c) 2021 TU Berlin FF
-%
 function [all_wpts_out, lat, lon, lat_wpts, lon_wpts] = process_lateral_profile(all_wpts)
 
-%   Constants
-KTS2MPS	= (1.852/3.6);
-MPS2KTS = (3.6/1.852);
-FT2M = 0.3048;
-M2FT = (1/0.3048);
-NM2M = 1852.0;
-M2NM = (1/1852.0);
-G_CONST	= 9.80665;
-DEG2RAD = (pi/180.0);
-RAD2DEG = (180.0/pi);
+% Define constants used in conversions
+KTS2MPS	= (1.852/3.6);       % Knots to meters per second
+MPS2KTS = (3.6/1.852);       % Meters per second to knots
+FT2M = 0.3048;               % Feet to meters
+M2FT = (1/0.3048);           % Meters to feet
+NM2M = 1852.0;               % Nautical miles to meters
+M2NM = (1/1852.0);           % Meters to nautical miles
+G_CONST	= 9.80665;            % Gravitational constant
+DEG2RAD = (pi/180.0);        % Degrees to radians
+RAD2DEG = (180.0/pi);        % Radians to degrees
 
+% Prepare the output by copying the input waypoint data
 all_wpts_out = all_wpts;
 
+% Convert cell arrays to matrices for easier mathematical operations
 lat_wpts = cell2mat(all_wpts.lat);
 lon_wpts = cell2mat(all_wpts.lon);
  
-%   Compute the distances between the waypoints, 
-%   except for ARC --> then NaN
+% Compute the distances between the waypoints, except for ARC type waypoints
 for ind = 1:length(all_wpts.lat)-1
-    
+    % Convert lat/lon from degrees to radians
     pt1.lat = lat_wpts(ind)*pi/180;
     pt1.lon = lon_wpts(ind)*pi/180;
     
     pt2.lat = lat_wpts(ind+1)*pi/180;
     pt2.lon = lon_wpts(ind+1)*pi/180;
     
+    % Use the inverse function to calculate distance and course between two waypoints
     [all_wpts_out.dist_to_nxt{ind}, all_wpts_out.crs_to_nxt{ind}, crs21] = inverse(pt1.lat,pt1.lon,pt2.lat,pt2.lon);
-    
-    end
+end
 
+% For the last waypoint, set the course and distance to next as NaN
 ind = length(all_wpts.lat);
 all_wpts_out.crs_to_nxt{ind} = NaN;
 all_wpts_out.dist_to_nxt{ind} = NaN;
+
+% Initialize empty arrays for latitude and longitude values
 lat = [];
 lon = [];
 
-%disp(all_wpts.name);
-%disp(all_wpts.leg_type);
-
-
-%   Iterate over all generated waypoints
+% Iterate over all waypoints to calculate flight path segments
 for i = 1:length(all_wpts.name)-1
-    %wp = all_wpts.name{i};
-    %leg = all_wpts.leg_type{i};
-    %disp(wp);
-    %disp(leg);
-    
-
-%% last waypoint
+    % Handle the last waypoint separately
     if i == length(all_wpts.name)-1
-        fprintf("FINAL:...calculating ");
-        fprintf(all_wpts.leg_type{i});
-        fprintf("-leg from:");
-        fprintf(all_wpts.name{i});
-        fprintf("\n");
+        fprintf('FINAL:...calculating %s-leg from:%s\n', all_wpts.leg_type{i}, all_wpts.name{i});
 
-        % Check if lat and lon vectors are not empty
+        % If lat and lon arrays are not empty, use the last element. Otherwise, use waypoint position
         if isempty(lat) && isempty(lon)
-            % Save the last element of lat and lon in pt1.lat and pt1.lon
             pt1.lat = lat_wpts(i) * pi/180;
             pt1.lon = lon_wpts(i) * pi/180;
         else
-            % Use lat_wpts(i) and lon_wpts(i) if the vectors are empty
             pt1.lat = lat(end) * pi/180;
             pt1.lon = lon(end) * pi/180;
         end
-        
+
+        % Define position for the next waypoint
         pt2.lat = lat_wpts(i+1)*pi/180;
         pt2.lon = lon_wpts(i+1)*pi/180;
 
-        [lat_i, lon_i, dist12]=create_straight(pt1, pt2, 100);
+        % Create a straight flight path between two waypoints
+        [lat_i, lon_i, dist12] = create_straight(pt1, pt2, 100);
         lat = [lat, lat_i*180/pi];
         lon = [lon, lon_i*180/pi];
 
     else
-        % Check if lat and lon vectors are not empty
+        % If lat and lon arrays are not empty, use the last element. Otherwise, use current waypoint position
         if isempty(lat) && isempty(lon)
-            % use position of current wp
             pt1.lat = lat_wpts(i) * pi/180;
             pt1.lon = lon_wpts(i) * pi/180;
         else
-            % Use last calculated position of aircraft
             pt1.lat = lat(end) * pi/180;
             pt1.lon = lon(end) * pi/180;
         end
-
-        %assumption: all wp are fly-over, beside end of RF-leg
+        
+        % Define the coordinates of the second point (next waypoint)
         pt2.lat = lat_wpts(i+1)*pi/180; 
         pt2.lon = lon_wpts(i+1)*pi/180;
 
- %% Non-RF-leg cases
-        if all_wpts.leg_type{i+1} ~= "RF"
-            fprintf("Non-RF:...calculating ");
-            fprintf(all_wpts.leg_type{i+1});
-            fprintf("-leg to:");
-            fprintf(all_wpts.name{i});
-            fprintf("\n");
-
+        % Begin processing non-RF-leg cases
+        if all_wpts.leg_type{i+1} ~= 'RF'
+            % Inform about the leg type and name we are calculating
+            fprintf('Non-RF:...calculating %s-leg to: %s\n', all_wpts.leg_type{i+1}, all_wpts.name{i});
+        
+            % Define the coordinates of the third point (two waypoints ahead)
             pt3.lat = lat_wpts(i+2)*pi/180;
             pt3.lon = lon_wpts(i+2)*pi/180;
-    
-            %calc arc between wp-1, wp and wp+1
+            
+            % Calculate the distances and course between wp, wp+1 and wp+2
             [dist12, crs12, crs21] = inverse(pt1.lat,pt1.lon,pt2.lat,pt2.lon);
             [dist23, crs23, crs32] = inverse(pt2.lat,pt2.lon,pt3.lat,pt3.lon);
             [dist13, crs13, crs31] = inverse(pt1.lat,pt1.lon,pt3.lat,pt3.lon);
-
-            %check if course change is big enough for
-            %wgs84_tangent_fixed_radius_arc()
+        
+            % Check if course change is significant enough for wgs84_tangent_fixed_radius_arc()
             cours_change = abs(crs23-crs12);
-            if cours_change > 1e-2 && all_wpts.leg_type{i+2} ~= "RF"
-        
-                % Calculate arc
-                % Compute air density, air pressure, temperature and speed of sound
+            if cours_change > 1e-2 && all_wpts.leg_type{i+2} ~= 'RF'
+                
+                % Compute the atmospheric conditions
                 [rho, p, temp, soundSpeed] = Atmos(5000*FT2M);
-                % Compute the true airspeed based on height and CAS schedule
+                % Calculate true airspeed based on altitude and CAS schedule
                 V = CAStoTAS(220,rho,p)*KTS2MPS;
-                % Nominal bank angle of 22 degrees
+                % Calculate radius using nominal bank angle of 22 degrees
                 radius = V^2/(G_CONST*tan(22*DEG2RAD));
-    
+            
+                % Calculate arc between pt1, pt2, and pt3
                 [centerPt, startPt, endPt, dir] = wgs84_tangent_fixed_radius_arc(pt1, crs12, pt3, crs32-pi, radius);
-        
-                %calc dots between pt1 and startPt
+                
+                % Calculate coordinates between pt1 and startPt
                 [lat_i, lon_i, dist12]=create_straight(pt1, startPt, 50);
                 lat = [lat, lat_i*180/pi];
                 lon = [lon, lon_i*180/pi];
-            
-                %calc dots in arc between startPt and endPt
+                    
+                % Calculate coordinates in the arc between startPt and endPt
                 [lat_i, lon_i, dist12]=create_arc(startPt, endPt, centerPt, 50);
                 lat = [lat, lat_i*180/pi];
                 lon = [lon, lon_i*180/pi];
-        
             
-            else %course change too small or next leg == RF-leg
-                %calc dots between p1 and pt2
+            % If course change is too small or next leg is an RF-leg
+            else 
+                % Calculate coordinates between pt1 and pt2
                 [lat_i, lon_i, dist12]=create_straight(pt1, pt2, 100);
                 lat = [lat, lat_i*180/pi];
                 lon = [lon, lon_i*180/pi];
             end
-%% RF-leg case
+        
+        % Begin processing RF-leg cases
         else
-            fprintf("RF:...calculating ");
-            fprintf(all_wpts.leg_type{i+1});
-            fprintf("-leg to:");
-            fprintf(all_wpts.name{i});
-            fprintf("\n");
-
-                
+            % Inform about the RF leg type and name we are calculating
+            fprintf('RF:...calculating %s-leg to: %s\n', all_wpts.leg_type{i+1}, all_wpts.name{i});
+            
+            % Define coordinates of first point
             pt1.lat = lat_wpts(i) * pi/180;
             pt1.lon = lon_wpts(i) * pi/180;
-    
+            
+            % Define coordinates of the second point (next waypoint)
             pt2.lat = lat_wpts(i+1)*pi/180; 
             pt2.lon = lon_wpts(i+1)*pi/180;
-
-            pt3.lat = all_wpts.center_lat{i+1}*pi/180; %center
+        
+            % Define coordinates of the center point for the arc
+            pt3.lat = all_wpts.center_lat{i+1}*pi/180;
             pt3.lon = all_wpts.center_lon{i+1}*pi/180;
-
+        
+            % Calculate the coordinates on the arc between pt1, pt2, and pt3
             [lat_i, lon_i, dist12]=create_arc(pt1,pt2,pt3,100);
             lat = [lat, lat_i*180/pi];
             lon = [lon, lon_i*180/pi];
         end
-    end
-end % end of loop; 
-end % end of function:
-
+    end % End of loop
+end % End of function
 
 
